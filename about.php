b02e2075a380e059462fe689dac1e72c1a7040e8
Jnauq <justin.aq88@gmail.com>
2018-08-24 17:28:01 -0700

    edits to about

--- about.php
+++ about.php
@@ -11,24 +11,24 @@ require_once('view/top.php');
 
 <div class="about-us">
     <h2 class="about-h2">What is BCIT books?</h2>
-    <p class="about-desc">A platform for BCIT students to buy and sell used textbooks to each other.
+    <p class="about-desc">A platform for BCIT students to buy and sell used textbooks.
     </p>
 
     <h2 class="about-h2">How it works</h2>
     
     <div class="about-desc">
-        <p class="about-mission"><i>"How to post textbooks"</i></p>
+        <p class="about-mission"><i>How to post textbooks</i></p>
         <ol>
             <li>Upload a picture of your textbook(s)</li>
             <li>Fill out the details of the book(s)</li>
             <li>Enter an email address you wish to be contacted through</li>
             <li>Create a password so you can edit or delete your post later</li>
-            <li>Post it and wait for someone make a request<li>
+            <li>Post it and wait for someone make a request</li>
         </ol>
     </div>
 
     <div class="about-desc">
-        <p class="about-mission"><i>"How to request textbooks"</i></p>
+        <p class="about-mission"><i>How to request textbooks</i></p>
         <ol>
             <li>Search for the textbooks you are looking for</li>
             <li>Click the request button</li>
